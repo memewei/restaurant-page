a1a12fe6fc06cae050b2dc677591701399f62c60
memewei <minwei2002@gmail.com>
2023-09-12 12:14:17 +0800

    Write home module

---
+++ src/pika.js
@@ -0,0 +1,29 @@
+function createPika(){
+    const pika = document.createElement("div");
+    pika.classList.add("home");
+
+    const pikaPortrait = document.createElement("img");
+    pikaPortrait.src = "images/pika-portrait.png";
+    pikaPortrait.alt = "Pika";
+
+    pika.appendChild(createPara("Aloha! You must have followed your nose!"));
+    pika.appendChild(createPara("I'm Pika."));
+    pika.appendChild(pikaPortrait);
+    pika.appendChild(createPara("I own Pika's, an outdoor restaurant in the heart of the village."));
+
+    return pika;
+}
+
+function createPara(text){
+    const para = document.createElement("p");
+    para.textContent = text;
+    return para;
+}
+
+function loadPika(){
+    const main = document.getElementById("main");
+    main.textContent = "";
+    main.appendChild(createPika());
+}
+
+export default loadPika;
